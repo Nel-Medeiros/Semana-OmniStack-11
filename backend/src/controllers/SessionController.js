6af77b03d587bec1ba9cbe53344231b3e936f8c2
Nel-Medeiros <emanuelpaivamedeiros@gmail.com>
2020-03-27 13:07:10 -0300

    Backend Finalizado

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,18 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async create(request, response) {
+        const { id } = request.body;
+
+        const ong = await connection('ongs')
+            .where('id', id)
+            .select('name')
+            .first();
+
+        if (!ong) {
+            return response .status(400).json({ error: 'Não foi localizada nenhuma ONG com esse ID.'})
+        }
+
+        return response.json(ong);
+    }
+}
